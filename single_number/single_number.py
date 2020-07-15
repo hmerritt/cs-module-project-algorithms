ef3e7a0b2a47ca285f3ce5d70e891d62bc18ac77
Harry Merritt <harry@merritt.es>
2020-07-15 01:04:17 +0100

    Complete single_number function

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,14 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    # Sort array
+    arr.sort()
 
-    pass
+    # If next value (in a sorted array) is not equal to current -> bingo!
+    # Only check even indexes and use last index if none found
+    for i, val in enumerate(arr):
+        if i == len(arr) - 1 or (i % 2 == 0 and arr[i] != arr[i+1]):
+            return val
 
 
 if __name__ == '__main__':
